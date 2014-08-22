c87383673ad2e8c5522283043d223662dfa9cef8
JonathanMELIUS <JonathanMELIUS@users.noreply.github.com>
2014-08-22 15:32:11 +0200

    Delete ObserverSidePanel.java

--- src/org/pathvisio/tissueanalyzer/ObserverSidePanel.java
+++
@@ -1,7 +0,0 @@
-package org.pathvisio.tissueanalyzer;
-
-import java.util.ArrayList;
-
-public interface ObserverSidePanel {
-	public void update(ArrayList<String> progress,ArrayList<String> selected);
-}
